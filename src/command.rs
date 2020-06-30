79d530f072f53ca9bc2b2486be4efb14fb28d32e
Naoki Ikeguchi <root@siketyan.dev>
2020-06-30 23:19:32 +0900

    Remove test command

--- src/command.rs
+++ src/command.rs
@@ -1,20 +1,17 @@
 mod add;
 mod list;
 mod serve;
-mod test;
 
 use add::AddCommand;
 use clap::Clap;
 use list::ListCommand;
 use serve::ServeCommand;
-use test::TestCommand;
 
 use crate::Opts;
 
 #[derive(Clap)]
 pub enum Command {
     Add(AddCommand),
-    Test(TestCommand),
     List(ListCommand),
     Serve(ServeCommand),
 }
@@ -25,7 +22,6 @@ pub trait ICommand {
 
 pub fn execute(opts: Opts) {
     match &opts.command {
-        Command::Test(command) => command.execute(&opts),
         Command::List(command) => command.execute(&opts),
         Command::Add(command) => command.execute(&opts),
         Command::Serve(command) => command.execute(&opts),
