4fca5504a7e4212137bffb73a430e1f2249ae2c0
Naoki Ikeguchi <root@siketyan.dev>
2020-07-13 00:37:07 +0900

    string -> buffer

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -3,12 +3,12 @@ use std::io::{BufWriter, Read, Write};
 
 pub(crate) fn open(path: &str) -> String {
     let mut file = File::open(path).expect("Failed to open the file.");
-    let mut string = String::new();
+    let mut buffer = String::new();
 
-    file.read_to_string(&mut string)
+    file.read_to_string(&mut buffer)
         .expect("Failed to read from the file.");
 
-    string
+    buffer
 }
 
 pub(crate) fn save(path: &str, content: String) {
