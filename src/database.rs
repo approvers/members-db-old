6c528c27c1e54df0e58dc626692712ac786ca214
Naoki Ikeguchi <root@siketyan.dev>
2020-07-13 00:49:26 +0900

    Update current database definition

--- src/database.rs
+++ src/database.rs
@@ -1,35 +1,40 @@
-pub(crate) mod member;
+pub mod member;
 
 use member::Member;
+use serde::{Deserialize, Serialize};
 
 use crate::filesystem;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct Payload {
+    members: Vec<Member>,
+}
 
 #[derive(Debug)]
 pub struct Database {
     path: String,
-    members: Vec<Member>,
+    payload: Payload,
 }
 
 impl Database {
     pub fn new(path: String) -> Self {
         let yaml = filesystem::open(path.as_str());
-        let members: Vec<Member> =
-            serde_yaml::from_str(yaml.as_str()).expect("Failed to deserialize.");
+        let payload: Payload = serde_yaml::from_str(yaml.as_str()).expect("Failed to deserialize.");
 
-        Database { path, members }
+        Database { path, payload }
     }
 
     pub fn save(&self) {
-        let yaml = serde_yaml::to_string(&self.members).expect("Failed to serialize.");
+        let yaml = serde_yaml::to_string(&self.payload).expect("Failed to serialize.");
 
         filesystem::save(&self.path, yaml)
     }
 
     pub fn get_members(&self) -> &Vec<Member> {
-        &self.members
+        &self.payload.members
     }
 
     pub fn add_member(&mut self, member: Member) {
-        self.members.push(member)
+        self.payload.members.push(member)
     }
 }
